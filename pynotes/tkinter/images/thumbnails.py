cfb0df2dc65a92f0e73a2a16483274d5db9fbd25
Kevin <keyvin@desktop>
2014-09-29 10:50:03 -0400

    more notes

---
+++ pynotes/tkinter/images/thumbnails.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""
+display all images in a directory as thumbnail image buttons that display 
+the full image when clicked; requires PIL for JPEGs and thumbnail image
+creation; to do: add scrolling if too many are there
+""" 
--- pynotes/tkinter/sliders/slider1.py
+++ pynotes/tkinter/sliders/slider1.py
@@ -9,9 +9,29 @@ class Demo(Frame):
         self.var=IntVar()
         valhorizontal = Label(self, text='0', width=5)
         valvertical = Label(self, text='0', width= 5)
-        slideh = Slider(self, label='Pick demo', command self.onMove, variable=self.var, from_=0, to=len(demos)-1, length=200, tickinterval=1, showvalue=YES, orient='horizontal')
-        slidev = Slider(self, label='Pick demo', command self.onMove, variable=self.var, from_=0, to=len(demos)-1, length=200, tickinterval=1, showvalue=YES, orient, 'vertical')
+        slideh = Scale(self, label='Pick demo', command=self.onMove, variable=self.var, from_=0, to=len(demos)-1, length=200, tickinterval=1, showvalue=YES, orient='horizontal')
+        slidev = Scale(self, label='Pick demo', command=self.onMove, variable=self.var, from_=0, to=len(demos)-1, length=200, tickinterval=1, showvalue=YES, orient='vertical')
+        slideh.pack(side=TOP)
+        slidev.pack(side=RIGHT)
         Quitter(self).pack(side=RIGHT)
         Button(self, text='Run Demo', command=self.runner).pack(side=LEFT)
         Button(self, text='State', command=self.getstate).pack(side=RIGHT)
         
+    def onMove(self, event):
+        print("Slider Moved")
+
+    def runner(self):
+        pos = self.var.get()
+        print("You picked %s"%pos)
+        demo = list(demos.values())[pos]
+        demo()
+
+
+    def getstate(self):
+        print(self.var.get())
+
+
+if __name__ == '__main__':
+    Demo().mainloop()
+              
+    
