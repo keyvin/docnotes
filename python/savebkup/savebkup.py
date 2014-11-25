c9f715d687646d007e8efbacabb8572e46b8f54c
Kevin Price <keyvin@gmail.com>
2014-11-24 20:57:14 -0500

    work

--- python/savebkup/savebkup.py
+++ python/savebkup/savebkup.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shelve
 from tkinter import *
 
 
@@ -32,6 +33,26 @@ class savedlg(Frame):
         pass
     def cpybtn(self):
         pass
+
+#This class loads and saves state for the file list, maintains a list of files in the last backup. For ease of use, uses a shelve
+class filelist():
+    def __init__(self, path=''):
+        if path:
+            self.path = path
+        else:
+            self.path = './savelist.shlf'
+        self.shelve=shelve.open(self.path)
+    def getlist(self):
+        retlist = []
+        for i in self.shelve.keys():
+            retlist.append((i,shelve[i]))
+        return retlist
+    def addlist(self, key, value):
+        self.shelve[key] = value
+    def closelist(self):
+        self.shelve.close()
+        
+                
 
 if __name__=='__main__':
     savedlg().pack(expand=True, fill=BOTH)
