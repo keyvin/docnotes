445555a3d4fb3ec4bdb4f9dec07927e6a4c505d4
Kevin <keyvin@gmail.com>
2014-11-25 14:04:32 -0500

    Added a new dialog, need to work on layout

--- python/savebkup/savebkup.py
+++ python/savebkup/savebkup.py
@@ -2,7 +2,7 @@ import sys
 import os
 import shelve
 from tkinter import *
-
+from tkinter.filedialog import askopenfilename
 
 class savedlg(Frame):
     def __init__(self, parent=None, **opts):
@@ -23,17 +23,47 @@ class savedlg(Frame):
         self.delbutton.pack(side=LEFT)
         self.copybutton.pack(side=LEFT)
         self.listbox.pack(side=LEFT, expand=True, fill=BOTH)
-        self.sbar.pack(side=RIGHT, expand=True, fill=Y)
+        self.sbar.pack(side=LEFT, expand=False, fill=Y)
         self.lstframe.pack(side=TOP, expand=True, fill=BOTH)
         self.btnframe.pack(side=BOTTOM, expand=False , fill=None)
 
     def addbtn(self):
-        pass
+        adddialog = adddlg()
+        self.wait_window(adddialog.toplevel)
+		
     def delbtn(self):
         pass
     def cpybtn(self):
         pass
 
+class adddlg():
+	def __init__(self, parent=None, **opts):
+		self.toplevel = Toplevel(parent, opts)
+		self.frame = Frame(self.toplevel)
+		self.pathentry = Entry(self.toplevel, width=80, text='Select File or Directory')
+		self.nameentry = Entry(self.toplevel, width=80, text='Enter name of game or file')
+		self.selbutton = Button(self.toplevel, text='...', command=self.selbutton)		
+		self.okbutton = Button(self.toplevel, text='Ok', command=self.okbutton)
+		self.pathentry.pack()
+		self.nameentry.pack()
+		self.selbutton.pack()
+		self.okbutton.pack()
+		self.toplevel.protocol('WM_DELETE_WINDOW', self.toplevel.destroy)
+		self.file =  ''
+		self.description = ''
+		self.toplevel.grab_set()
+	def okbutton(self):
+		self.file = self.pathentry.get()
+		self.description = self.nameentry.get()
+		self.toplevel.destroy()
+	def selbutton(self):
+		self.file = askopenfilename()
+		self.pathentry.delete(0, END)
+		self.pathentry.insert(0, self.file)
+		
+		
+		
+		
 #This class loads and saves state for the file list, maintains a list of files in the last backup. For ease of use, uses a shelve
 class filelist():
     def __init__(self, path=''):
@@ -55,5 +85,8 @@ class filelist():
                 
 
 if __name__=='__main__':
-    savedlg().pack(expand=True, fill=BOTH)
-    mainloop()
+	savedlg().pack(expand=True, fill=BOTH)
+	savedlist = filelist()
+	savedlist.closelist()
+	mainloop()
+	
