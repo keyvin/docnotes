f5c263289186f28fd66e1a5e8c3f4b9ba6b68744
Kevin <keyvin@gmail.com>
2014-12-13 09:30:58 -0500

    Moving right along

---
+++ python/bsdb/maingui.py
@@ -0,0 +1,42 @@
+from tkinter import * 
+
+class RootWindow(Frame):
+    def __init__(self, parent=None, **opts):
+        Frame.__init__(self, parent, opts)
+        self.browse_btn = Button(self, text="Browse all", command=self.browse)
+        self.brs_kwd_btn = Button(self, text="Browse Keywords", command=self.keywd)
+        self.brs_auth = Button(self, text="Browse Authors", command=self.auths)
+        self.srch_auth = Button(self, text="Search Authors", command=self.sauths)
+        self.srch_std = Button(self, text="Search Studies", command=self.s_studs)
+        self.srch_kywd = Button(self, text="Search Keywords", command=self.s_kywd)
+        self.add_std = Button(self, text="Add Study", command=self.a_std)
+        self.add_auth = Button(self, text="Add Author", command = self.a_auth)
+        self.browse_btn.pack(side=TOP, expand=True, fill=BOTH)
+        self.brs_kwd_btn.pack(side=TOP, expand=True, fill=BOTH)
+        self.brs_auth.pack(side=TOP, expand=True, fill=BOTH)
+        self.srch_auth.pack(side=TOP, expand=True, fill=BOTH)
+        self.srch_std.pack(side=TOP, expand=True, fill=BOTH)
+        self.srch_kywd.pack(side=TOP, expand=True, fill=BOTH)
+        self.add_std.pack(side=TOP, expand=True, fill=BOTH)
+        self.add_auth.pack(side=TOP, expand=True, fill=BOTH)
+
+    def browse(self):
+        pass
+    def keywd(self):
+        pass
+    def auths(self):
+        pass
+    def sauths(self):
+        pass
+    def s_studs(self):
+        pass
+    def s_kywd(self):
+        pass
+    def a_std(self):
+        pass
+    def a_auth(self):
+        pass
+
+if __name__=='__main__':
+    RootWindow().pack(expand=True, fill=BOTH)
+    mainloop()
--- python/savebkup/savebkup.py
+++ python/savebkup/savebkup.py
@@ -1,76 +1,99 @@
 import sys
 import os
+import os.path
 import shelve
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+import tkinter.messagebox
 import tkinter.filedialog
+import shutil
+import distutils.dir_util
+
 
 class SaveDlg(Frame):
-    def __init__(self, parent=None, **opts):
+    def __init__(self, list=None, parent=None, **opts):
         Frame.__init__(self, parent,  opts)
-        self.lstframe = Frame(self)
-        self.listbox = Listbox(self.lstframe)
-        self.sbar = Scrollbar(self.lstframe)
-        self.sbar.config(command=self.listbox.yview)
-        self.listbox.config(yscrollcommand=self.sbar.set)
+        self.list=list
+        self.lst_frame = Frame(self)
+        self.list_box = Listbox(self.lst_frame, selectmode=SINGLE)
+        self.sbar = Scrollbar(self.lst_frame)
+        self.sbar.config(command=self.list_box.yview)
+        self.list_box.config(yscrollcommand=self.sbar.set)
         
         #need a scrollbar for listbox. Need to link the two
-        self.btnframe = Frame(self)
+        self.btn_frame = Frame(self)
         
-        self.addbutton = Button(self.btnframe, text='Add', command=self.add_btn)
-        self.delbutton = Button(self.btnframe, text='Delete', command=self.del_btn)
-        self.copybutton = Button(self.btnframe, text='Copy', command=self.cpy_btn)
-        self.addbutton.pack(side=LEFT)
-        self.delbutton.pack(side=LEFT)
-        self.copybutton.pack(side=LEFT)
-        self.listbox.pack(side=LEFT, expand=True, fill=BOTH)
+        self.add_button = Button(self.btn_frame, text='Add', command=self.add_btn)
+        self.del_button = Button(self.btn_frame, text='Delete', command=self.del_btn)
+        self.copy_button = Button(self.btn_frame, text='Copy', command=self.cpy_btn)
+        self.add_button.pack(side=LEFT)
+        self.del_button.pack(side=LEFT)
+        self.copy_button.pack(side=LEFT)
+        self.list_box.pack(side=LEFT, expand=True, fill=BOTH)
         self.sbar.pack(side=LEFT, expand=False, fill=Y)
-        self.lstframe.pack(side=TOP, expand=True, fill=BOTH)
-        self.btnframe.pack(side=BOTTOM, expand=False , fill=None)
-
+        self.lst_frame.pack(side=TOP, expand=True, fill=BOTH)
+        self.btn_frame.pack(side=BOTTOM, expand=False , fill=None)
+        self.load_list(list)
+    def load_list(self, list):
+        for i in list.shelve.keys():
+            self.list_box.insert(END, i)
     def add_btn(self):
         add_dialog = AddDlg()
         self.wait_window(add_dialog.top_level)
-        self.listbox.insert(0, add_dialog.description)
-		#do something here with the data from that dialog
+        if not add_dialog.description:
+            return
+        if self.list.in_list(add_dialog.description):
+            if not tkinter.messagebox.askokcancel("Already in the list, replace?"):
+                return
+            self.list.add_to_list(add_dialog.description, add_dialog.file)
+            return
+        self.list_box.insert(END, add_dialog.description)
+        #Just use the description as the key on the shelve. 
+        self.list.add_to_list(add_dialog.description, add_dialog.file)
+
     def del_btn(self):
-        pass
+        if self.list_box.curselection():
+            self.list.delete_key(self.list_box.get(self.list_box.curselection()))
+            self.list_box.delete(self.list_box.curselection())
+			#delete from a shelve.
+		
     def cpy_btn(self):
-        pass
+        copier(self.list, 'C:\\Users\\keyvi_000\\Google Drive\\saves\\')
 
 class AddDlg():
-	def __init__(self, parent=None, **opts):
-		self.top_level = Toplevel(parent, opts)
-		self.file_frame = Frame(self.top_level)
-		
-		self.lbl_1 = Label(self.file_frame, text='Select File Name')
-		self.path_entry = Entry(self.file_frame, width=40, text='Select File or Directory')
-		self.name_entry = Entry(self.top_level, width=40, text='Enter name of game or file')
-		self.sel_file_button = Button(self.file_frame, text='Open File', command=self.sel_file_button)		
-		self.sel_dir_button = Button(self.file_frame, text='Open Directory', command=self.sel_dir_button)
-		self.ok_button = Button(self.top_level, text='Ok', command=self.ok_button)
-		self.path_entry.pack(side=LEFT, expand=True, fill=X)		
-		self.sel_file_button.pack(side=LEFT, expand=False)
-		self.sel_dir_button.pack(side=LEFT, expand=False)
-		self.file_frame.pack(side=TOP, expand=True, fill=X)
-		self.name_entry.pack(side=TOP, expand=True, fill=X)
-		self.ok_button.pack(side=BOTTOM)
-		self.top_level.protocol('WM_DELETE_WINDOW', self.top_level.destroy)
-		self.file =  ''
-		self.description = ''
-		self.top_level.grab_set()
-	def ok_button(self):
-		self.file = self.path_entry.get()
-		self.description = self.name_entry.get()
-		self.top_level.destroy()
-	def sel_file_button(self):
-		self.file = tkinter.filedialog.askopenfilename()
-		self.path_entry.delete(0, END)
-		self.path_entry.insert(0, self.file)
-	def sel_dir_button(self):
-		self.file = tkinter.filedialog.askdirectory()
-		self.path_entry.delete(0, END)
-		self.path_entry.insert(0, self.file)
+    def __init__(self, parent=None, **opts):
+        self.top_level = Toplevel(parent, opts)
+        self.file_frame = Frame(self.top_level)	
+        self.lbl_1 = Label(self.file_frame, text='Select File Name')
+        self.path_entry = Entry(self.file_frame, width=40, text='Select File or Directory')
+        self.name_entry = Entry(self.top_level, width=40, text='Enter name of game or file')
+        self.sel_file_button = Button(self.file_frame, text='Open File', command=self.sel_file_button)		
+        self.sel_dir_button = Button(self.file_frame, text='Open Directory', command=self.sel_dir_button)
+        self.ok_button = Button(self.top_level, text='Ok', command=self.ok_button)
+        self.path_entry.pack(side=LEFT, expand=True, fill=X)		
+        self.sel_file_button.pack(side=LEFT, expand=False)
+        self.sel_dir_button.pack(side=LEFT, expand=False)
+        self.file_frame.pack(side=TOP, expand=True, fill=X)
+        self.name_entry.pack(side=TOP, expand=True, fill=X)
+        self.ok_button.pack(side=BOTTOM)
+        self.top_level.protocol('WM_DELETE_WINDOW', self.top_level.destroy)
+        self.file =  ''
+        self.description = ''
+        self.top_level.grab_set()
+        self.path_entry.delete(0,END)
+        self.name_entry.delete(0, END)
+    
+    def ok_button(self):
+        self.file = self.path_entry.get()
+        self.description = self.name_entry.get()
+        self.top_level.destroy()
+    def sel_file_button(self):
+        self.file = tkinter.filedialog.askopenfilename()
+        self.path_entry.delete(0, END)
+        self.path_entry.insert(0, self.file)
+    def sel_dir_button(self):
+        self.file = tkinter.filedialog.askdirectory()
+        self.path_entry.delete(0, END)
+        self.path_entry.insert(0, self.file)
 		
 		
 		
@@ -82,21 +105,39 @@ class FileList():
         else:
             self.path = './savelist.shlf'
         self.shelve=shelve.open(self.path)
-    def getlist(self):
+    def get_list(self):
         retlist = []
         for i in self.shelve.keys():
             retlist.append((i,shelve[i]))
         return retlist
-    def addlist(self, key, value):
+    def add_to_list(self, key, value):
         self.shelve[key] = value
-    def closelist(self):
+    def delete_key(self, key):
+        if key in self.shelve.keys():
+            del self.shelve[key]
+    def close_list(self):
         self.shelve.close()
-        
-                
+    def get_file(self, key):
+        if key in self.shelve.keys():
+            return self.shelve[key]
+        else:
+            return None
+    def in_list(self, key):
+        return (key in self.shelve.keys())
+
+def copier(list, targetdir):
+        for key in list.shelve.keys():
+            to_copy = list.get_file(key)
+            if os.path.exists(to_copy):
+                #shutil.copy(to_copy, targetdir)
+                if os.path.isdir(to_copy):
+                    distutils.dir_util.copy_tree(to_copy, targetdir+'\\'+key+'\\')
+                if os.path.isfile(to_copy):
+                    shutil.copy(to_copy, targetdir)
+					
 
 if __name__=='__main__':
-	SaveDlg().pack(expand=True, fill=BOTH)
-	SavedList = FileList()
-	SavedList.closelist()
+	saved_list = FileList()
+	SaveDlg(saved_list).pack(expand=True, fill=BOTH)
 	mainloop()
-	
+	saved_list.close_list()
