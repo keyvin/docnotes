53956a376966c536d197a2a57705b5297e7a83c1
Not Me <keyvin@gmail.com>
2015-09-21 13:31:21 -0700

    Added del, remove

--- python/savebkup2/main.py
+++ python/savebkup2/main.py
@@ -1,13 +1,17 @@
 from tkinter import *
 import os
 import pickle
-
+import tkinter.filedialog
+import tkinter.simpledialog
+import tkinter.messagebox
 global save_path
 save_path = './save.pkl'
+#data struct - {Pairname:[Sourcepath, destpath, isdir]}
+
 class mainWin(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.loadPickle()
+        
         self.t_label = Label(self, text="Save Mover")
         self.listbox_frame = Frame(self)
         self.list_box = Listbox(self.listbox_frame)
@@ -27,15 +31,42 @@ class mainWin(Tk):
         self.add_button.pack(side=LEFT)
         self.move_button.pack(side=RIGHT)
         self.protocol('WM_DELETE_WINDOW', self.close_handler)
+        self.loadPickle()
         
-        for i in self.dir_list.keys():
-            self.list_box.insert(END, i)
+        # # for i in self.dir_list.keys():
+            # self.list_box.insert(END, i)
     def add(self):
-        pass
+        name = tkinter.simpledialog.askstring("Name of game", "What is the name you want this pair to have?")
+        isdir = tkinter.messagebox.askquestion(title="Direcrtory?", message="Is the source a directory?")
+        
+        if isdir == 'yes':
+            isdir = True
+            source = tkinter.filedialog.askdirectory(title="wWhat is the source folder")
+        else:
+            isdir = False
+            source = tkinter.filedialog.askopenfilename(title="File to move")
+        
+        destination = tkinter.filedialog.askdirectory(title="Destination?")
+        
+        finalok = tkinter.messagebox.askokcancel(title="Create pair?", message= "Pair named: " + str(name) + "\n" + "Source "+ str(source) + "\n" + "Destination" + str(destination) )
+        # print(finalok)
+        if finalok:
+            self.addpair(name, source, destination, isdir )
+            
     def delete(self):
-        pass
+        active = self.list_box.get(ACTIVE)
+        info = self.dir_list[active]
+        delete_it = tkinter.messagebox.askokcancel(title = "Remove game?", message="Remove " + str(active) + "from save list?")
+        if delete_it == True:
+            del(self.dir_list[active])
+            self.list_box.delete(ACTIVE)
     def move(self):
         pass
+    def addpair(self, name, source, destination, isdir):
+        self.dir_list[name] = [source, destination, isdir]
+        self.addtolistbox(name)
+    def addtolistbox(self, name):
+        self.list_box.insert(END, name)
     def close_handler(self):
         file = open(save_path, 'wb')
         pickle.dump(self.dir_list, file)
@@ -46,9 +77,15 @@ class mainWin(Tk):
             file = open(save_path,'rb')
             self.dir_list = pickle.load(file)
             file.close()
+            print(self.dir_list)
+            for i in self.dir_list.keys():
+                print(i)
+                self.addtolistbox(i)
         else:
-            self.dir_list = {'Name':'Dir'}
+            self.dir_list = {}
             
+# class 
+			
 if __name__ == "__main__":
     mainWin().mainloop()
     
