7195857c4d8f1e5bb1b34723813674489ba8091d
keyvin <keyvin@gmail.com>
2018-07-27 01:00:47 -0400

    characters

--- curses/base.py
+++ curses/base.py
@@ -12,7 +12,8 @@ class window():
     def __keypress__(self, key):
         pass
     
-    def draw(self, screen)
+    def draw(self, screen):
+        pass
 
 class subreddit_window(window):
     def __init__(self):
@@ -27,6 +28,24 @@ class subreddit_window(window):
 
 
 if __name__ == "__main__":    
+    #Init curses library
     screen = curses.initscr()
-    pass
-    
+    curses.noecho()
+    screen.addch(10,10,curses.ACS_ULCORNER)
+    screen.addch(11,10,curses.ACS_LLCORNER)
+    screen.addch(10,11,curses.ACS_URCORNER)
+    screen.addch(11,11,curses.ACS_LRCORNER)
+    #curses.ACS_VLINE
+    #curses.ACS_HLINE
+    screen.refresh()
+    screen.keypad(True)
+    screen.getch()
+    curses.nocbreak()
+    screen.keypad(False)
+    curses.endwin()
+
+
+
+
+
+
