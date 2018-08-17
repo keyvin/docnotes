a3d367bf4a0624088ec22bbf4e98b9132eae362b
kevin <keyvin@gmail.com>
2018-08-16 21:48:52 -0400

    world_1

---
+++ games/map.h
@@ -0,0 +1,31 @@
+#ifndef MAP_H
+#define MAP_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define P_NORTH 0x01
+#define P_EAST 0x02
+#define P_SOUTH 0x04
+#define P_WEST 0x08
+
+#define 
+
+typedef struct CELL {
+  ;
+  
+} map_cell;
+
+typedef struct MAP {
+  unsigned int visited;
+  unsigned int x_size;
+  unsigned int y_size;
+  
+  
+
+}
+
+
+
+
+#endif
