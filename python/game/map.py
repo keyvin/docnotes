ba5c7aa7aa03e34d77e1562d5bef5a1a892c07ce
keyvin <keyvin@gmail.com>
2018-08-19 17:05:33 -0400

    base

--- python/game/map.py
+++ python/game/map.py
@@ -1,9 +1,8 @@
 import copy
 
+map_commands = {"north": (0, -1), "south": (0, 1), "east": (1, 0), "west": (-1, 0)}
 
-
-
-cell_defaults = {"north": True, "south": True, "east": True, "west": True, "special": 0, "message": 0}
+cell_defaults = {"north": True, "south": True, "east": True, "west": True, "special": 0, "message": 0, "type": grass}
 
 class cell:
     def __init__(self,  **kwargs):
@@ -11,15 +10,11 @@ class cell:
         self.properties = copy.deepcopy(cell_defaults)
         for i in kwargs.keys():
             self.properties[i] = kwargs[i]
-        
-        
-        
-
-
+                
 class map:
     def __init__(self, **kwargs):
-        self.x_size = 10
-        self.y_size = 10
+        self.x_size = 100
+        self.y_size = 100
         self.grid = [[cell() for x in range(self.x_size)] for y in range(self.y_size)]
         pass
 
@@ -27,14 +22,25 @@ class map:
         pass
 
     def navigate(self, command, party_obj):
-        pass
+       
+        curr_x = party_obj.location_x
+        curr_y = party_obj.location_y
 
+        curr_x = curr_x + map_commands[command][0]
+        curr_y = curr_y + map_commands[command][1]
+        if curr_x < 0 or curr_x > self.x_size:
+            pass
+            #xfault
+        if curr_y < 0 or curr_y > self.y_size:
+            pass
+            #yfault
+        if party_obj.passable(self.grid[x][y]):
+            pass
+            #travel fault
 
+        #move was allright
+        party_obj.location_x = curr_x
+        party_obj.location_y = curr_y
+        return
     
-
-
-
-    
-
-
 
---
+++ python/game/party.py
@@ -0,0 +1,2 @@
+#this file will have code for party management, moving the party, etc.
+
