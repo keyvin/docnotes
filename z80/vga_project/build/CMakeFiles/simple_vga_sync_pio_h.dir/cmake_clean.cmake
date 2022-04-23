d39b194644af259ff6288ec122d2247f30b875db
kevin <keyvin@gmail.com>
2022-04-23 01:00:43 -0400

    video mode (640x480 text working

--- z80/vga_project/build/CMakeFiles/simple_vga_sync_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/simple_vga_sync_pio_h"
-  "sync.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/simple_vga_sync_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- z80/vga_project/build/CMakeFiles/simple_vga_z80io_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/simple_vga_z80io_pio_h"
-  "z80io.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/simple_vga_z80io_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
