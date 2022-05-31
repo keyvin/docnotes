7534e82bd2f1822fb1ccc25c060a7403df048a20
kevin <keyvin@gmail.com>
2022-05-31 11:54:28 -0400

    Pinout corrected for schematic

--- z80/vga_project/vga_charrom.c
+++ z80/vga_project/vga_charrom.c
@@ -204,7 +204,7 @@ uint sm_z80io;
 
 void z80io_setup() {
   //  stdio_init_all();
-  float freq = 40000000.0;
+  float freq = 80000000.0;
   float div = (float)clock_get_hz(clk_sys) / freq;
   //gpio_set_dir(13,0);
   p1 = pio1;
@@ -214,13 +214,13 @@ void z80io_setup() {
   gpio_init(12);
   gpio_init(11);
   gpio_init(10);
-  gpio_set_dir(10,GPIO_OUT);	
-  gpio_set_dir(11,GPIO_IN);
-  gpio_put(10,1);
-  gpio_set_dir(12,GPIO_IN);
-  gpio_pull_up(11);
-  gpio_pull_down(12);
-  //for (int i = 0; i < 8; i++) gpio_set_dir(14+i,0);
+  gpio_set_dir(11,GPIO_OUT);	
+  gpio_set_dir(10,GPIO_IN);
+  gpio_put(11,1);
+  gpio_set_dir(12,GPIO_IN);  
+  gpio_pull_up(12);
+//  gpio_pull_down(10);  
+//for (int i = 0; i < 8; i++) gpio_set_dir(14+i,0);
   z80io_init(p1, sm_z80io, offset_z80io, div);
   pio_sm_set_enabled(p1, sm_z80io, true);    
 }
@@ -255,7 +255,8 @@ void bus_read() {
     r1 = pio_sm_get(p1,sm_z80io);
     base = (uint8_t)((r1 & 0x0000FF00) >> 8);		  
     //	printf("(in) %d, base - %d, val - %d, count - %d\r\n", r1, base, regs[base],r4++ );
-    r1 = regs[base];
+    if (cursor!=0) r1 = sbuffer[cursor-1];
+    else r1=sbuffer[2399];
     r2 = r1 << 24 | r1 << 16 | r1 <<8 | r1;		  
     pio_sm_put(p1, sm_z80io, r2);
     pio_interrupt_clear(p1,6);				
